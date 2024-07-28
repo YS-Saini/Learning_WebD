5c7859854a9a28883e293e5935b1db559e33a05d
YS-Saini <yssaini2004@gmail.com>
2024-07-28 13:37:17 +0530

    Added one more button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("myButton").addEventListener("click", function() {
     alert("Button Clicked!");
 });
+
+document.getElementById('nextpage').addEventListener('click', function() {
+    window.location.href = 'next_page.html';
+});
